4422b7577c4740643f585347c325e5f6da76ceb9
bartol567 <bartol567@gmail.com>
2024-04-16 00:03:52 +0200

    Ticket Seller

--- TicketSellerAPI/Program.cs
+++ TicketSellerAPI/Program.cs
@@ -1,25 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using TicketSellerAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add Swagger generation with the necessary configurations.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policyBuilder =>
+    {
+        policyBuilder.AllowAnyOrigin()
+                     .AllowAnyMethod()
+                     .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddDbContext<TicketSellerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TicketSellerDB")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket Seller API V1");
+        c.RoutePrefix = string.Empty;  // Serve the Swagger UI at the app's root (e.g., http://localhost:5000/)
+    });
 }
-
+app.UseCors("AllowAll");
+// Middleware to redirect HTTP requests to HTTPS.
 app.UseHttpsRedirection();
-
+// Middleware to enable authorization.
 app.UseAuthorization();
-
+// Maps controllers to their respective routes.
 app.MapControllers();
-
+// Runs the application.
 app.Run();
